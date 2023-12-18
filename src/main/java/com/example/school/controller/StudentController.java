719e891fa0b6f653a8916012dd8c6689f5adce06
vineethsankre <lsvineethsankre@gmail.com>
2023-12-18 10:15:38 +0000

    First commit

---
+++ src/main/java/com/example/school/controller/StudentController.java
@@ -0,0 +1,8 @@
+/*
+ *
+ * You can use the following import statemets
+ *
+ * import org.springframework.web.bind.annotation.*;
+ * import java.util.*;
+ *
+ */
---
+++ src/main/java/com/example/school/model/StudentRowMapper.java
@@ -0,0 +1,10 @@
+/*
+ * You can use the following import statements
+ *
+ * import java.sql.ResultSet;
+ * import java.sql.SQLException;
+ * import org.springframework.jdbc.core.RowMapper;
+ *
+ */
+
+// Write your code here
