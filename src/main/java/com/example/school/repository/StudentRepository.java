ecabe1e4ccb03b214b758871323430c88dbbc8bb
vineethsankre <lsvineethsankre@gmail.com>
2023-12-18 10:31:21 +0000

    StudentRepository.java created

--- src/main/java/com/example/school/repository/StudentRepository.java
+++ src/main/java/com/example/school/repository/StudentRepository.java
@@ -1,1 +1,15 @@
-// Write your code here
+package com.example.school.repository;
+
+import com.example.school.model.*;
+import java.util.*;
+
+public interface StudentRepository{
+    ArrayList<Student> addStudent();
+
+    Student getStudentById(int studentId);
+
+    Student addStudent(Student student);
+
+    Student updateStudent(int studentId, Student student);
+    void deleteStudent(int studentId);
+}
