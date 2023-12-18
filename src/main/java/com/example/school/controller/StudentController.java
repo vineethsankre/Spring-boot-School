681c26a8d6b305562d5bb21ee783e2754db178b7
vineethsankre <lsvineethsankre@gmail.com>
2023-12-18 10:50:13 +0000

    StudentController.java created'

--- src/main/java/com/example/school/controller/StudentController.java
+++ src/main/java/com/example/school/controller/StudentController.java
@@ -1,8 +1,39 @@
-/*
- *
- * You can use the following import statemets
- *
- * import org.springframework.web.bind.annotation.*;
- * import java.util.*;
- *
- */
+package com.example.school.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+import com.example.school.model.Student;
+import com.example.school.service.StudentH2Service;
+
+@RestController
+public class StudentController {
+    @Autowired
+    StudentH2Service studentH2Service;
+
+    @GetMapping("/students")
+    public ArrayList<Student> addStudent() {
+        return studentH2Service.addStudent();
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudentById(@PathVariable("studentId") int studentId) {
+        return studentH2Service.getStudentById(studentId);
+    }
+
+    @PostMapping("/students")
+    public Student addStudent(@RequestBody Student student) {
+        return studentH2Service.addStudent(student);
+    }
+
+    @PutMapping("/students/{studentId}")
+    public Student updateStudent(@PathVariable int studentId, @RequestBody Student student) {
+        return studentH2Service.updateStudent(studentId, student);
+    }
+
+    @DeleteMapping("/todos/{id}")
+    public void deleteStudent(@PathVariable int studentId) {
+        studentH2Service.deleteStudent(studentId);
+    }
+}
